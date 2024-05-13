51470396ddfc7e19c9e05393ea2bd945fc777273
Wen-Wei Tseng <sosiristseng@gmail.com>
2024-05-13 14:52:26 +0800

    fix hessian_sparsity

--- docs/08-symbolics.jl
+++ docs/08-symbolics.jl
@@ -2,7 +2,7 @@
 
 # Symbolic calculations in Julia
 
-`Symbolics.jl` is a computer Algebra System (CAS) for Julia. The symbols are number-like and follow Julia semantics so we can put them into a regular function to get a symbolic counterpart.
+`Symbolics.jl` is a computer Algebra System (CAS) for Julia. The symbols are number-like and follow Julia semantics so we can put them into a regular function to get a symbolic counterpart. Symbolics.jl is the backbone of ModelingToolkit.jl.
 
 Source:
 
@@ -37,9 +37,11 @@ using Latexify
 x^2 + y^2
 
 # You can use Latexify to see the LaTeX code.
-A = [x^2+y 0 2x
+A = [
+    x^2+y 0 2x
     0 0 2y
-    y^2+x 0 0]
+    y^2+x 0 0
+]
 
 latexify(A)
 
@@ -69,9 +71,15 @@ ex = x^2 + y^2 + sin(x)
 isequal(2ex, ex + ex)
 
 #---
+isequal(simplify(2ex), simplify(ex + ex))
+
+#---
 ex / ex
 
-# Symblic integration: use [SymbolicNumericIntegration.jl](https://github.com/SciML/SymbolicNumericIntegration.jl). The [Youtube video](https://youtu.be/L47k2zjPU9s) by `doggo dot jl` gives a concise example.
+
+#===
+Symblic integration: use [SymbolicNumericIntegration.jl](https://github.com/SciML/SymbolicNumericIntegration.jl). The [Youtube video](https://youtu.be/L47k2zjPU9s) by `doggo dot jl` gives a concise example.
+===#
 
 #===
 ## Custom functions used in Symbolics
@@ -94,31 +102,35 @@ xs[1]
 # Explicit vector form
 collect(xs)
 
+# Operations on arrays are supported
+sum(collect(xs))
+
 #===
 
 ## Example: Rosenbrock function
+
 Wikipedia: https://en.wikipedia.org/wiki/Rosenbrock_function
-We use the vector form of Rosenbrock function
+We use the vector form of Rosenbrock function.
 
 ===#
 
-rosenbrock(xs) =
-    sum(1:length(xs)-1) do i
-        100 * (xs[i+1] - xs[i]^2)^2 + (1 - xs[i])^2
-    end
+rosenbrock(xs) = sum(1:length(xs)-1) do i
+    100 * (xs[i+1] - xs[i]^2)^2 + (1 - xs[i])^2
+end
 
 # The function is at minimum when xs are all one's
 rosenbrock(ones(100))
 
 #---
-N = 100
+N = 20
 @variables xs[1:N]
 
-# A long list of vector components
+# A full list of vector components
 xs = collect(xs)
 
 #---
 rxs = rosenbrock(xs)
+
 
 # Gradient
 grad = Symbolics.gradient(rxs, xs)
@@ -133,8 +145,8 @@ hes2 = Symbolics.hessian(rxs, xs)
 isequal(hes1, hes2)
 
 # ### Sparse matrix
-# Sparse Hessian matrix of the Hessian matrix of the Rosenbrock function w.r.t. to vector components. (298 non-zero values out of 10000 elements)
-hes_sp = Symbolics.hessian_sparsity(rxs, xs)
+# Sparse Hessian matrix of the Hessian matrix of the Rosenbrock function w.r.t. to vector components.
+hes_sp = Symbolics.hessian_sparsity(rosenbrock, xs)
 
 # Visualize the sparse matrix with `Plots.spy()`
 using Plots
@@ -169,7 +181,6 @@ isapprox(foop(inxs), out)
 
 # You can save the generated function for later use: `write("function.jl", string(fexprs[2]))`
 # Here, `ForwardDiff.jl` checks if our gradient generated from `Symbolics.jl` is correct.
-
 using ForwardDiff
 ForwardDiff.gradient(rosenbrock, inxs) ≈ out
 
